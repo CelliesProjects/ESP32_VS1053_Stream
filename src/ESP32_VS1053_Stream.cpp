790115c7426a42a2742176710fa0515b774ca308
3KU_Delta <48127424+3KUdelta@users.noreply.github.com>
2022-09-01 12:11:23 +0200

    adding setTone() for Bass and Treble

--- src/ESP32_VS1053_Stream.cpp
+++ src/ESP32_VS1053_Stream.cpp
@@ -417,6 +417,10 @@ void ESP32_VS1053_Stream::setVolume(const uint8_t vol) {
     if (_vs1053 && !_startMute) _vs1053->setVolume(_volume);
 }
 
+void ESP32_VS1053_Stream::setTone(uint8_t *rtone) {
+     _vs1053->setTone(rtone);
+}
+
 String ESP32_VS1053_Stream::currentCodec() {
     return mimestr[_currentMimetype];
 }
