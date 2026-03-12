647d81f58faa99a6ae9a9cf78ea5c1fe5e25877c
Cellie <CelliesProjects@users.noreply.github.com>
2026-03-12 16:39:36 +0100

    Typo

--- src/ESP32_VS1053_Stream.cpp
+++ src/ESP32_VS1053_Stream.cpp
@@ -326,7 +326,7 @@ bool ESP32_VS1053_Stream::connecttohost(const char *url, const char *username,
         if (audio_showstation && !_http->header(ICY_NAME).equals(""))
             audio_showstation(_http->header(ICY_NAME).c_str());
 
-        _remainingBytes = _http->getSize(); // -1 when Server sends no contentType-Length header (chunked streams)
+        _remainingBytes = _http->getSize(); // -1 when Server sends no Content-Length header (chunked streams)
         _chunkedResponse = _http->header(ENCODING).equalsIgnoreCase("chunked") ? true : false;
         _offset = (_remainingBytes == -1) ? 0 : offset;
         _metaDataStart = _http->header(ICY_METAINT).toInt();
