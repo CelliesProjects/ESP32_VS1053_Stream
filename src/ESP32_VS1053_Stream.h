69d170b80354c999b7484ba6bd7d9e2a3d14e826
Cellie <CelliesProjects@users.noreply.github.com>
2026-08-17 19:52:03 +0200

    Default to `true`

--- src/ESP32_VS1053_Stream.h
+++ src/ESP32_VS1053_Stream.h
@@ -102,7 +102,7 @@ public:
         e.g. uint8_t rtone[4]  = {12, 15, 15, 15}; // initialize bass & treble
         See https://www.vlsi.fi/fileadmin/datasheets/vs1053.pdf section 9.6.3 */
 
-    bool playChunk(uint8_t *data, size_t len, bool stopSong = false);
+    bool playChunk(uint8_t *data, size_t len, bool stopSong = true);
 
 private:
     VS1053 *_vs1053;
