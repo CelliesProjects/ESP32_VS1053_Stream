394f0389b24bc33c0b8b36644f795ba597a303d9
Cellie <CelliesProjects@users.noreply.github.com>
2026-09-01 16:08:35 +0200

    Cleanup

--- src/ESP32_VS1053_Stream.cpp
+++ src/ESP32_VS1053_Stream.cpp
@@ -1516,7 +1516,7 @@ bool ESP32_VS1053_Stream::_playChunkNB()
         return true;
 
     size_t bytesToDecoder = 0;
-    while (_vs1053->data_request() && _chunkRemaining && bytesTodecoder < 512)
+    while (_vs1053->data_request() && _chunkRemaining && bytesToDecoder < 512)
     {
         const size_t len = min(VS1053_PLAYBUFFER_SIZE, _chunkRemaining);
         _vs1053->playChunk(_chunk, len);
